b61ccef3fbb4a2c83e328ea7ff33340117fadee6
Anca Vulpe <ancavulpe@ancavulpe.waw.corp.google.com>
2017-07-14 15:21:13 +0200

    test for GCF

---
+++ examples/helloWorld/index.js
@@ -0,0 +1,9 @@
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloHttp = function helloHttp2 (req, res) {
+  res.send(`Hello ${req.body.name || 'World'}!Nice to be here! :-)`);
+};
