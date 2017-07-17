d1dd40b804150af0c33675c933bb8ba398e71173
Avulpe <ancavulpe@google.com>
2017-07-17 11:39:00 +0200

    Update index.js

--- examples/helloWorld/index.js
+++ examples/helloWorld/index.js
@@ -4,6 +4,6 @@
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.helloHttp = function helloHttp2 (req, res) {
+exports.helloHttp2 = function helloHttp2 (req, res) {
   res.send(`Hello ${req.body.name || 'World'}! Nice to be here ! :-) `);
 };
