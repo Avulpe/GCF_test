df1f6c087bd4e46854193c9a1ca1f63f015f8f96
Avulpe <ancavulpe@google.com>
2017-09-21 13:53:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,12 @@
 const translate = require('@google-cloud/translate')();
  
 exports.translateHttp = function translateHttp (req, res) {
-if (!req.text) {
-        throw new Error('Text not provided. Make sure you have a "text" property in your request');
-      }
-      if (!req.lang) {
-        throw new Error('Language not provided. Make sure you have a "lang" property in your request');
-      }
+
 
       const options = {
         from: "ro",
         to: "en"
       };
 
-  res.send(translate.translate(req.text, options));
+  res.send(translate.translate(req.body.text, options));
 };
